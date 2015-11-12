ac3395968d5b5440d77ecf3ccf619cad0ee4d940
13280025 <13280025@localhost>
2015-11-12 17:57:52 -0200

    adicionada libraries do jcommon e jchart

---
+++ trabTecnicas/src/trabtecnicas/meioPagamento.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabtecnicas;
+
+/**
+ *
+ * @author 13280025
+ */
+public interface meioPagamento {
+    
+    double saldo();
+    
+}
